ba1d597023abb6fa60529d6abece18a43f359e11
David Ibáñez <d.ibanez03@ufromail.cl>
2024-05-30 14:08:42 -0400

    Add<Project>: Creación componente Header
    Se crea y agrega un header al proyecto.

---
+++ src/components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+    <header class="top-bar">
+        <div class="title-header">
+            <button class="principal-title-button" @click="$router.push('/')">Blog Pakicetus</button>
+        </div>
+    </header>
+</template>
+
+<script setup>
+</script>
+
+<style>
+.title-header {
+    display: flex;
+    justify-content: center;
+}
+.principal-title-button {
+    font-size: 45px;
+    border: none;
+    border-radius: 25px;
+    background-color: rgb(153, 231, 240);
+    margin: 15px;
+    padding: 15px;
+}
+</style>
